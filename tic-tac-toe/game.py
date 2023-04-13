02d352ed52de057eb680d98e9a0bf428cdc502ff
igor@laptop <iszymanow@student.ethz.ch>
2023-04-13 11:33:59 +0200

    added an efficient random player and tqdm bar

--- tic-tac-toe/game.py
+++ tic-tac-toe/game.py
@@ -3,20 +3,20 @@ from sarsa import Sarsa
 import threading
 import numpy as np
 import matplotlib.pyplot as plt
+from tqdm import tqdm
+from player0 import Random_Player
 
 def main():
     game_inst = env2.Env()
     p2,p3 = Sarsa(-1, game_inst, 0.2, 0.1, 1), Sarsa(1, game_inst, 0.2, 0.1, 1)
-    
+    rand = Random_Player(-1, game_inst)
     # wins=[]
     # loses=[]
     # draws=[]
     
     if __name__ == '__main__':
-        for i in range(10000):
-            print("game:",str(i+1) + "/10000")
-            
-            p = threading.Thread(target=p2.play)
+        for i in tqdm(range(10000)):
+            p = threading.Thread(target=rand.play)
             q = threading.Thread(target=p3.play)
             p.start()
             q.start()
@@ -24,7 +24,7 @@ def main():
             p.join()
             q.join()
 
-
+            # print(game_inst.render())
             game_inst.reset()
             # wins.append(game_inst.p1_wins)
             # loses.append(game_inst.p0_wins)
--- tic-tac-toe/player0.py
+++ tic-tac-toe/player0.py
@@ -1,18 +1,51 @@
 import numpy as np
 
-class player:
+class Random_Player:
     """
-    class implements a dummy player which just plays random moves
+    class that implements a dummy player which just plays random moves
     """
-
-    def __init__(self):
-        self.sign = -1
-        self.last_action = [-1,-1]
+    def __init__(self, sym, env):
+        self.sym = sym
+        self.env = env
+        self.last_move = None
         
 
-    def move(legal_moves, state):
-        index = np.random.randint(len(legal_moves))
-        return legal_moves[index]
 
-    def reward(reward):
-        pass
+    def move(self, state, legal):
+        """
+        the function which takes the current state, as well as the legal moves and
+        chooses a random move out of the available ones.
+        If the game is finished, it returns a tuple (-1,-1).
+        """
+        if self.env.isTerminated(state, legal): #if the game is already finished, just return a dummy output
+            return (-1,-1)
+        else: # choose a random move from the available ones
+            index = np.random.randint(len(legal))
+            return legal[index]
+
+
+    
+    def play(self):
+        """
+        the playing function of the algorithm. It makes use of the environment's condition variable,
+        to synchronize with the other player. The player runs the loop until the game terminates"""
+        while True:
+            with self.env.cv:
+                while self.env.turn != self.sym:
+                    self.env.cv.wait()
+
+                legal, state = self.env.get_obs()
+                self.last_move = self.move(state, legal)
+                self.env.action(self.last_move)
+
+                self.env.cv.notify()
+            
+            if self.last_move == (-1,-1):
+                break
+
+
+
+
+
+
+
